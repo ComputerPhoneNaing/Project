6f1044896135e86f70daaec94f261b957c342eb0
ComputerPhoneNaing <khantphonenaing156@gmail.com>
2024-04-19 17:37:36 +0630

    After testing monment.minjs and adding bundle.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-console.log("HEllo JS");
-let day = moment().startOf('day').fromNow();
-console.log(day);
+let moment = require("moment")
+var unique = require("uniq");
+
+var myDate = new Date()
+var myCoolDate = moment(myDate).format('LL')
+
+var myList = [1,2,3,3,6,5,7]
+var uniqueList = unique(myList)
+console.log(uniqueList);
+
+console.log(myCoolDate);
